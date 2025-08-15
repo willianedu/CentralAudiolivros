ee9abbcebda5a8d7e235a7920013e085993d021a
Willian Eduardo <willianeduardot@outlook.com>
2025-08-15 14:22:26 -0300

    Fix HTML and Vite configuration: update stylesheet paths and add importmap for dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
+
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig, loadEnv } from 'vite';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
